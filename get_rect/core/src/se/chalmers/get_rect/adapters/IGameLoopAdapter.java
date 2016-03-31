5f7016b6d8acce713f1f904e4def5348b16379ee
Simon <ssundstrom@live.se>
2016-03-31 15:36:13 +0200

    Added files for adapters and entites

---
+++ get_rect/core/src/se/chalmers/get_rect/adapters/IGameLoopAdapter.java
@@ -0,0 +1,7 @@
+package se.chalmers.get_rect.adapters;
+
+/**
+ * Created by simsund on 2016-03-31.
+ */
+public interface IGameLoopAdapter {
+}
---
+++ get_rect/core/src/se/chalmers/get_rect/entities/controller/IEntity.java
@@ -0,0 +1,8 @@
+package se.chalmers.get_rect.entities.controller;
+
+/**
+ * Created by simsund on 2016-03-31.
+ */
+public interface IEntity {
+
+}
