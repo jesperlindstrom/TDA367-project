372af1ec89ddbdbf7481972a0c1d660b2e61488c
Jesper Lindström <raxezdev@gmail.com>
2016-04-03 14:07:00 +0200

    Added junit and first test

---
+++ get_rect/core/test/se/chalmers/get_rect/ExampleTest.java
@@ -0,0 +1,11 @@
+package se.chalmers.get_rect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void testExample() {
+        assertEquals("A", "B");
+    }
+}
