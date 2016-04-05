4c5acd8cb3cc3378e2f7926c73470dfcc56195da
Jesper Lindström <raxezdev@gmail.com>
2016-04-05 17:13:35 +0200

    JSON strategy progress

--- get_rect/core/src/se/chalmers/get_rect/io/IOStrategy.java
+++ get_rect/core/src/se/chalmers/get_rect/io/IOStrategy.java
@@ -1,8 +1,9 @@
 package se.chalmers.get_rect.io;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface IOStrategy<T> {
-    List<T> read(String file);
+    List<T> read(String file) throws FileNotFoundException;
     void write(String file, List<T> data);
 }
--- get_rect/core/src/se/chalmers/get_rect/io/json/JsonIOStrategy.java
+++ get_rect/core/src/se/chalmers/get_rect/io/json/JsonIOStrategy.java
@@ -1,13 +1,21 @@
 package se.chalmers.get_rect.io.json;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import se.chalmers.get_rect.io.IOStrategy;
 
+import java.io.*;
 import java.util.List;
 
 public class JsonIOStrategy<T> implements IOStrategy<T> {
     @Override
-    public List<T> read(String file) {
-        return null;
+    public List<T> read(String file) throws FileNotFoundException {
+        InputStream stream = new FileInputStream(file);
+        Reader reader = new InputStreamReader(stream);
+        Gson gson = new Gson();
+
+        return gson.fromJson(reader, new TypeToken<List<T>>(){}.getType());
     }
 
     @Override
