1cc4cb43360b72915fe60b88461bc9d51bff3ab2
SamuelHakansson <samuel.hakansson@live.com>
2016-04-05 14:53:47 +0200

    copied player features to zombie

--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieController.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieController.java
@@ -1,5 +1,28 @@
 package se.chalmers.get_rect.game.entities.enemies.zombie;
 
 
-public class ZombieController {
+import se.chalmers.get_rect.adapters.IGraphicsAdapter;
+import se.chalmers.get_rect.game.entities.IPhysicsController;
+import se.chalmers.get_rect.game.entities.IView;
+
+public class ZombieController implements IPhysicsController {
+    private Zombie model;
+    private IView view;
+
+    public ZombieController(Zombie zombie, IView view) {
+        this.model = model;
+        this.view = view;
+    }
+    @Override
+    public void update(long delta) {
+
+    }
+
+    @Override
+    public void draw(IGraphicsAdapter graphics) {view.draw(graphics);}
+
+    @Override
+    public void getBoundingBox() {
+
+    }
 }
--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieView.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieView.java
@@ -1,5 +1,19 @@
 package se.chalmers.get_rect.game.entities.enemies.zombie;
 
 
-public class ZombieView {
+import se.chalmers.get_rect.adapters.IGraphicsAdapter;
+import se.chalmers.get_rect.game.entities.IView;
+import se.chalmers.get_rect.game.entities.player.Player;
+
+public class ZombieView implements IView{
+    private Zombie model;
+
+    public ZombieView(Zombie model){
+
+    }
+
+    @Override
+    public void draw(IGraphicsAdapter graphics) {
+
+    }
 }
