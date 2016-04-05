bb680562aeeea4fdfefe7e346975faa75f0f15a2
Felix Jansson <felixja@student.chalmers.se>
2016-04-05 14:35:26 +0200

    ?

--- get_rect/core/src/se/chalmers/get_rect/game/entities/ISolidEntity.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/ISolidEntity.java
@@ -2,4 +2,6 @@ package se.chalmers.get_rect.game.entities;
 
 import se.chalmers.get_rect.physics.ISolidObject;
 
-public interface ISolidEntity extends IEntity, ISolidObject { }
+public interface ISolidEntity extends IEntity, ISolidObject {
+
+}
