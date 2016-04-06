717a9cfd5744676702150698a74d3a235d71976d
Jesper Lindström <raxezdev@gmail.com>
2016-04-06 13:48:39 +0200

    Updated zombie data store

--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieDataStore.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieDataStore.java
@@ -3,10 +3,14 @@ package se.chalmers.get_rect.game.entities.enemies.zombie;
 import se.chalmers.get_rect.io.DataStore;
 
 public class ZombieDataStore implements DataStore<ZombieController> {
+    private static ZombieFactory factory = new ZombieFactory();
+
+    private int x;
+    private int y;
 
     @Override
     public ZombieController extract() {
-        return null;
+        return factory.make(x, y);
     }
 
     @Override
--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieFactory.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieFactory.java
@@ -1,11 +1,13 @@
 package se.chalmers.get_rect.game.entities.enemies.zombie;
 
-
-import se.chalmers.get_rect.game.entities.IController;
 import se.chalmers.get_rect.game.entities.IView;
 
 public class ZombieFactory {
-    public IController make() {
+    public ZombieController make() {
+        return make(0, 0);
+    }
+
+    public ZombieController make(int x, int y) {
         Zombie model = new Zombie();
         IView view = new ZombieView(model);
 
