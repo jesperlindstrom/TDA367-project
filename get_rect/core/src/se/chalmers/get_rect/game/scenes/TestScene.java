86bf7040a140d3f1dc72e9f50638eb9707a9d7f2
Simon <ssundstrom@live.se>
2016-04-06 14:03:02 +0200

    added zombie to TestScene

--- get_rect/core/src/se/chalmers/get_rect/game/scenes/TestScene.java
+++ get_rect/core/src/se/chalmers/get_rect/game/scenes/TestScene.java
@@ -1,11 +1,18 @@
 package se.chalmers.get_rect.game.scenes;
 
 import se.chalmers.get_rect.adapters.IGraphicsAdapter;
+import se.chalmers.get_rect.game.entities.IController;
+import se.chalmers.get_rect.game.entities.enemies.zombie.ZombieFactory;
 import se.chalmers.get_rect.game.entities.player.PlayerController;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TestScene implements IScene {
     private PlayerController playerController;
+    private List<IController> enemies;
 
     public TestScene(PlayerController playerController) {
         this.playerController = playerController;
@@ -14,6 +21,10 @@ public class TestScene implements IScene {
     @Override
     public void update(long delta) {
         playerController.update(delta);
+        for (IController entity: enemies) {
+            entity.update(delta);
+        }
+
     }
 
     @Override
@@ -21,12 +32,18 @@ public class TestScene implements IScene {
         graphics.start();
         graphics.draw("data/background.png", 0, 0);
         playerController.draw(graphics);
+        for (IController entity: enemies) {
+            entity.draw(graphics);
+        }
         graphics.end();
     }
 
     @Override
     public void enteringState(String previousStateName) {
-
+        playerController.setPosition(5, 90);
+        enemies = new ArrayList<IController>();
+        ZombieFactory zombieFactory = new ZombieFactory();
+        enemies.add(zombieFactory.make(60, 90));
     }
 
     @Override
