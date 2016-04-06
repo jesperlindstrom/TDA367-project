8906b6f61e24437afb97348861268c5fffab669d
Jesper Lindström <raxezdev@gmail.com>
2016-04-06 16:57:20 +0200

    Clean up

--- get_rect/core/src/se/chalmers/get_rect/physics/FrostbiteEngine.java
+++ get_rect/core/src/se/chalmers/get_rect/physics/FrostbiteEngine.java
@@ -3,11 +3,9 @@ package se.chalmers.get_rect.physics;
 import java.util.List;
 
 public class FrostbiteEngine implements IPhysicsEngine {
-
     private List<ISolidObject> entities;
 
     public FrostbiteEngine(List<ISolidObject> entities){
         this.entities = entities;
     }
-
 }
