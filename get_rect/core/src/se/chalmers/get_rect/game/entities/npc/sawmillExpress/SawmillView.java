28040740e2b2b456f54acea2b1bb850ee5eb9cbd
Simon <ssundstrom@live.se>
2016-04-11 16:59:38 +0200

    beeem me upp shake

--- get_rect/core/src/se/chalmers/get_rect/game/entities/npc/sawmillExpress/SawmillView.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/npc/sawmillExpress/SawmillView.java
@@ -1,15 +1,12 @@
 package se.chalmers.get_rect.game.entities.npc.sawmillExpress;
 
 import se.chalmers.get_rect.adapters.IGraphicsAdapter;
-import se.chalmers.get_rect.game.entities.IModel;
 import se.chalmers.get_rect.game.entities.IView;
 import se.chalmers.get_rect.utilities.Point;
 
-/**
- * Created by simsund on 2016-04-11.
- */
 public class SawmillView implements IView{
     private SawmillExpress model;
+    private int bla;
 
     public SawmillView(SawmillExpress SawmillExpress) {
         model = SawmillExpress;
@@ -17,9 +14,15 @@ public class SawmillView implements IView{
 
     @Override
     public void draw(IGraphicsAdapter graphics) {
-        graphics.draw("img/entities/sawmill/sawmill-express.png", model.getPosition());
+        bla++;
         if (model.isWaaah()) {
-            graphics.drawText("Wäääh", model.getPosition().add(new Point(50, 0)));
+            graphics.drawText("Wäääh", model.getPosition().add(new Point(50, 60)));
         }
+        if (model.isWaaah() && bla%2 == 1) {
+            graphics.draw("img/entities/sawmill/sawmill-express.png", model.getPosition().addX(5));
+        } else {
+            graphics.draw("img/entities/sawmill/sawmill-express.png", model.getPosition());
+        }
+
     }
 }
