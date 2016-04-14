a8aded4ae2b98d204bb365988cec054b71594d04
Simon <ssundstrom@live.se>
2016-04-14 15:42:59 +0200

    work done in zmobie

--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/Zombie.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/Zombie.java
@@ -3,13 +3,16 @@ package se.chalmers.get_rect.game.entities.enemies.zombie;
 
 import se.chalmers.get_rect.adapters.IRectangleAdapter;
 import se.chalmers.get_rect.adapters.IRectangleFactoryAdapter;
+import se.chalmers.get_rect.game.entities.IModel;
 import se.chalmers.get_rect.game.entities.IPhysicsModel;
+import se.chalmers.get_rect.game.entities.player.PlayerController;
 import se.chalmers.get_rect.physics.ISolidObject;
 import se.chalmers.get_rect.utilities.Point;
 
 public class Zombie implements IPhysicsModel {
     private static final int WIDTH = 100;
     private static final int HEIGHT = 100;
+    private IModel player;
     private Point position;
     private IRectangleAdapter boundingBox;
     private int health;
@@ -18,50 +21,22 @@ public class Zombie implements IPhysicsModel {
     private boolean walking;
     private double velocity;
 
-    public Zombie(IRectangleFactoryAdapter rectangleFactory){
-        this(new Point(0 ,0), rectangleFactory);
-    }
-
-    public Zombie(Point point, IRectangleFactoryAdapter rectangleFactory){
+    public Zombie(Point point, IRectangleFactoryAdapter rectangleFactory, IModel player){
         this.position = point;
         this.boundingBox = rectangleFactory.make(position.getX(), position.getY(), WIDTH, HEIGHT);
+        this.player = player;
         damage = 2;
         health = 5; //temp values
     }
 
-
+    @Override
     public IRectangleAdapter getBoundingBox() {
-        return boundingBox;
+        return null;
     }
 
     @Override
     public void onCollision(ISolidObject otherObject) {
 
-    }
-
-    public int getX() {
-        return position.getX();
-    }
-
-    public int getY() {
-        return position.getY();
-    }
-
-
-    public void setX(int x) {
-        position = position.setX(x);
-        boundingBox.setPosition(position);
-    }
-
-    public void setY(int y) {
-        position = position.setY(y);
-        boundingBox.setPosition(position);
-    }
-
-
-    public void setPosition(int x, int y) {
-        position = position.setPosition(x, y);
-        boundingBox.setPosition(position);
     }
 
     @Override
