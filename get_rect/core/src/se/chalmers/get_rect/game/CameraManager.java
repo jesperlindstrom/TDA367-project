343477c956aac75f683e450d2a7848de8c4faaba
Felix Jansson <felixja@student.chalmers.se>
2016-04-14 19:22:42 +0200

    Camera follows IModel

--- get_rect/core/src/se/chalmers/get_rect/game/CameraManager.java
+++ get_rect/core/src/se/chalmers/get_rect/game/CameraManager.java
@@ -4,29 +4,31 @@ package se.chalmers.get_rect.game;
 import se.chalmers.get_rect.GameConfig;
 import se.chalmers.get_rect.adapters.ICameraAdapter;
 import se.chalmers.get_rect.adapters.IGraphicsAdapter;
+import se.chalmers.get_rect.game.entities.IModel;
+import se.chalmers.get_rect.game.entities.IPhysicsModel;
 import se.chalmers.get_rect.game.entities.player.PlayerController;
 import se.chalmers.get_rect.utilities.Point;
 
 public class CameraManager implements IGameComponent{
 
     private ICameraAdapter cameraAdapter;
-    private PlayerController playerController;
+    private IModel model;
     private Point playerPos;
     private Point cameraPos;
 
 
-    public CameraManager(ICameraAdapter cameraAdapter, PlayerController playerController){
+    public CameraManager(ICameraAdapter cameraAdapter, IModel model){
         this.cameraAdapter = cameraAdapter;
-        this.playerController = playerController;
+        this.model = model;
         cameraPos = new Point(0,300);
-        playerPos = playerController.getPosition();
+        playerPos = model.getPosition();
         //Fix cameras first position
         cameraAdapter.translate(cameraPos);
     }
 
     @Override
     public void update(double delta) {
-        playerPos = playerController.getPosition();
+        playerPos = model.getPosition();
         moveX(delta);
         moveY(delta);
         cameraAdapter.update(delta);
@@ -38,13 +40,13 @@ public class CameraManager implements IGameComponent{
     }
 
     private void moveY(double delta){
-        if(playerPos.getX() != playerController.getPosition().getX() && !playerController.isJumping()){
+        if(playerPos.getX() != model.getPosition().getX()){
             move(cameraPos.deltaY(playerPos), 100, new Point(0,3));
         }
     }
 
     private Point deltaToVelocityX(double delta){
-        int velocity = (int)(PlayerController.MOVEMENT_SPEED * delta);
+        int velocity = (int)( delta);
         return new Point(velocity,0);
     }
 
