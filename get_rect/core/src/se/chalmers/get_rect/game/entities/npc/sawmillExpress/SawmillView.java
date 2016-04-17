5f29a67e1667f850bbcd99ad6519ae431bd5efa4
Jesper Lindström <raxezdev@gmail.com>
2016-04-17 19:40:38 +0200

    Sawmill uses animation

--- get_rect/core/src/se/chalmers/get_rect/game/entities/npc/sawmillExpress/SawmillView.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/npc/sawmillExpress/SawmillView.java
@@ -1,40 +1,32 @@
 package se.chalmers.get_rect.game.entities.npc.sawmillExpress;
 
 import se.chalmers.get_rect.adapters.IGraphicsAdapter;
+import se.chalmers.get_rect.game.entities.AbstractAnimatedView;
 import se.chalmers.get_rect.game.entities.IView;
 import se.chalmers.get_rect.utilities.Point;
 
-public class SawmillView implements IView{
+public class SawmillView extends AbstractAnimatedView {
+    private static final int DEFAULT = 1;
+    private static final int FLYING = 2;
     private SawmillExpress model;
-    private int bla;
 
-    public SawmillView(SawmillExpress SawmillExpress) {
-        model = SawmillExpress;
+    public SawmillView(SawmillExpress model) {
+        super(model, DEFAULT);
+        this.model = model;
+
+        addAnimationFrame(DEFAULT, "img/entities/sawmill/sawmill-express.png");
+        addAnimationFrame(FLYING, "img/entities/sawmill/sawmill-express-waaah.png");
     }
 
     @Override
     public void draw(IGraphicsAdapter graphics) {
-
-        Point pos = model.getPosition();
-
         if (model.isWäääh()) {
-            bla++;
-
-            if (bla > 40) {
-                graphics.drawText("Wäääh", model.getPosition().add(new Point(50, 60)));
-            }
-            if (bla % 2 == 1) {
-                pos = pos.addX(5);
-            }
-
-            if (bla%5 > bla %4) {
-                graphics.drawText("PARTY HARD", model.getPosition().add(-100, 150));
-            }
-
-            graphics.draw("img/entities/sawmill/sawmill-express-waaah.png", pos);
+            playSequence(FLYING);
+            graphics.drawText("Wäääh", model.getPosition().add(new Point(50, 60)));
         } else {
-            graphics.draw("img/entities/sawmill/sawmill-express.png", model.getPosition());
+            playSequence(DEFAULT);
         }
 
+        super.draw(graphics);
     }
 }
