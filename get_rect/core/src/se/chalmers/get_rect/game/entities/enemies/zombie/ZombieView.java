17e36fa6b625e3d805421c3a30957d25dd3f1e2d
Jesper Lindström <raxezdev@gmail.com>
2016-04-17 19:37:31 +0200

    ZombieView uses animation

--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieView.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/zombie/ZombieView.java
@@ -2,43 +2,20 @@ package se.chalmers.get_rect.game.entities.enemies.zombie;
 
 
 import se.chalmers.get_rect.adapters.IGraphicsAdapter;
-import se.chalmers.get_rect.game.entities.IView;
+import se.chalmers.get_rect.game.entities.AbstractAnimatedView;
 
-public class ZombieView implements IView{
-    private Zombie model;
-    private String oneLegImagePath;
-    private String twoLegImagePath;
-    private String currentImagePath;
-    private int imageWalkCount;
+public class ZombieView extends AbstractAnimatedView{
+    private static final int WALKING = 1;
 
     public ZombieView(Zombie model){
-        this.model = model;
-        this.oneLegImagePath = "img/entities/zombies/zombie.png";
-        this.twoLegImagePath = "img/entities/zombies/zombieOpen.png";
-        this.currentImagePath = twoLegImagePath;
-        this.imageWalkCount = 0;
-}
+        super(model, WALKING);
+
+        addAnimationFrame(WALKING, "img/entities/zombies/zombie.png", 5);
+        addAnimationFrame(WALKING, "img/entities/zombies/zombieOpen.png", 5);
+    }
 
     @Override
     public void draw(IGraphicsAdapter graphics) {
-        graphics.draw(currentImagePath, model.getPosition());
-        setImagePath();
-    }
-
-    /**
-     * Sets the player image to get an animation when the player is moving.
-     */
-    private void setImagePath() {
-        //// TODO: 2016-04-06 Might need some better values! this is to hard (coded), even for Lisch...
-        imageWalkCount++;
-        if (imageWalkCount <= 15) {
-
-            currentImagePath = twoLegImagePath;
-        } else {
-            if (imageWalkCount == 20) {
-                imageWalkCount = 0;
-            }
-            currentImagePath = oneLegImagePath;
-        }
+        super.draw(graphics);
     }
 }
