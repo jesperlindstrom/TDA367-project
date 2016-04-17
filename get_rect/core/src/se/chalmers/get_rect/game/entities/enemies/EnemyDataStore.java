134f1d51a555a1a5128cf50df745950895fa7d30
Jesper Lindström <raxezdev@gmail.com>
2016-04-17 17:10:22 +0200

    Removed use of IDataStore right now since it's not necessary at this point

--- get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/EnemyDataStore.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/enemies/EnemyDataStore.java
@@ -1,14 +1,12 @@
 package se.chalmers.get_rect.game.entities.enemies;
 
-import se.chalmers.get_rect.game.entities.IDataStore;
 import se.chalmers.get_rect.utilities.Point;
 
-public class EnemyDataStore implements IDataStore {
+public class EnemyDataStore {
     private int x;
     private int y;
     private String type;
 
-    @Override
     public Point getPosition() {
         return new Point(x ,y);
     }
