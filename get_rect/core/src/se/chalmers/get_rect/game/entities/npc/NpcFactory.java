df4bb23b242680814ee83f4999d42b9df113e68c
SamuelHakansson <samuel.hakansson@live.com>
2016-05-16 20:58:49 +0200

    merge

--- get_rect/core/src/se/chalmers/get_rect/game/entities/npc/NpcFactory.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/npc/NpcFactory.java
@@ -3,13 +3,10 @@ package se.chalmers.get_rect.game.entities.npc;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import se.chalmers.get_rect.game.entities.*;
-<<<<<<< HEAD
 import se.chalmers.get_rect.game.entities.npc.model.Rekoil;
 import se.chalmers.get_rect.game.entities.npc.view.RekoilView;
-=======
 import se.chalmers.get_rect.game.entities.npc.model.ChessT;
 import se.chalmers.get_rect.game.entities.npc.view.ChessTView;
->>>>>>> 6f26fd76f5f0792ffc7b1a1e4ebbd4e7ba445221
 import se.chalmers.get_rect.physics.IRectangleFactoryAdapter;
 import se.chalmers.get_rect.game.entities.npc.model.SawmillExpress;
 import se.chalmers.get_rect.game.entities.npc.view.SawmillView;
@@ -24,13 +21,11 @@ public class NpcFactory {
     public IPhysicsEntity make(String type, Point point) {
         if (type.equals("sawmillExpress"))
             return makeSawmillExpress(point);
-<<<<<<< HEAD
         if (type.equals("rekoil"))
             return makeRekoil(point);
-=======
         if (type.equals("chesst"))
             return makeChessT(point);
->>>>>>> 6f26fd76f5f0792ffc7b1a1e4ebbd4e7ba445221
+
 
         throw new EntityNotFoundException("NPC", type);
     }
@@ -41,16 +36,15 @@ public class NpcFactory {
 
         return new PhysicsEntity(model, view);
     }
-<<<<<<< HEAD
     private IPhysicsEntity makeRekoil(Point point) {
         Rekoil model = new Rekoil(point, rectangleFactory, dialogRepository);
         IView view = new RekoilView(model);
-=======
+        return new PhysicsEntity(model, view);
+    }
 
     private IPhysicsEntity makeChessT(Point point) {
         ChessT model = new ChessT(point, rectangleFactory, windowManager);
         IView view = new ChessTView(model);
->>>>>>> 6f26fd76f5f0792ffc7b1a1e4ebbd4e7ba445221
 
         return new PhysicsEntity(model, view);
     }
