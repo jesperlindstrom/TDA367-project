f33ccfa290f8aec79af9f4cfae08f261c1c30c40
Jesper Lindström <raxezdev@gmail.com>
2016-05-19 11:27:16 +0200

    Removed splash screen troll

--- get_rect/core/src/se/chalmers/get_rect/game/entities/window/view/SplashView.java
+++ get_rect/core/src/se/chalmers/get_rect/game/entities/window/view/SplashView.java
@@ -11,12 +11,6 @@ public class SplashView extends AbstractView {
     private Point cameraPos;
     private SplashModel model;
 
-    /**
-     * cameraPos adjusts to the current screen in use and fits the picture to the middle
-     * @param model
-     * @param camera
-     */
-
     public SplashView(SplashModel model, ICamera camera) {
         this.model = model;
         this.cameraPos = camera.getAdapter().getPosition();
@@ -24,27 +18,12 @@ public class SplashView extends AbstractView {
 
     @Override
     public void draw(IGraphicsAdapter graphics) {
-        if (model.getAddedAssets()) {
-            Point backgroundPos = cameraPos.add(-1920/2, -1080/2);
-            graphics.draw("img/splash/splash_bg.jpg", backgroundPos, GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT);
-            int progressWidth;
+        if (!model.getAddedAssets()) return;
 
-            if (model.getStop()) {
-                progressWidth = 578;
-            } else {
-                progressWidth = (int) (578 * model.getProgressValue());
-            }
+        Point backgroundPos = cameraPos.add(-1920/2, -1080/2);
+        graphics.draw("img/splash/splash_bg.jpg", backgroundPos, GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT);
 
-            graphics.draw("img/splash/loading_fill.png", backgroundPos.add(768, 128), progressWidth, 60);
-
-            if (model.getDidStop()) {
-                graphics.drawText("LOL JK xD", backgroundPos.add(1300, 200));
-            }
-
-            if (model.getProgressValue() > 2.3) {
-                int secondProgressWidth = (int) (300 * (model.getProgressValue() - 2.3));
-                graphics.draw("img/splash/loading_fill.png", backgroundPos.addY(128), secondProgressWidth, 60);
-            }
-        }
+        int progressWidth = (int) (578 * model.getProgressValue());
+        graphics.draw("img/splash/loading_fill.png", backgroundPos.add(768, 128), progressWidth, 60);
     }
 }
