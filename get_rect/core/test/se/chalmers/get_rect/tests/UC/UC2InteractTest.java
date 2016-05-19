e890771efee7a767a70db3361ef0a44ac0c0a844
Felix Jansson <felixja@student.chalmers.se>
2016-05-19 13:35:06 +0200

    test fixed, merge

--- get_rect/core/test/se/chalmers/get_rect/tests/UC/UC2InteractTest.java
+++ get_rect/core/test/se/chalmers/get_rect/tests/UC/UC2InteractTest.java
@@ -4,6 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import org.mockito.Mockito;
+import se.chalmers.get_rect.game.entities.DialogRepository;
+import se.chalmers.get_rect.game.entities.IRepository;
+import se.chalmers.get_rect.physics.IRectangleAdapter;
+import se.chalmers.get_rect.physics.IRectangleFactoryAdapter;
 import se.chalmers.get_rect.tests.physics.RectangleFactoryAdapterStub;
 import se.chalmers.get_rect.game.entities.enemies.model.Zombie;
 import se.chalmers.get_rect.game.entities.npc.NpcFactory;
@@ -13,25 +17,37 @@ import se.chalmers.get_rect.utilities.Point;
 import se.chalmers.get_rect.utilities.Side;
 import se.chalmers.get_rect.physics.CollisionData;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 
 public class UC2InteractTest {
 
     private Player player;
-    @Inject private NpcFactory npcFactory;
-    SawmillExpress sawmillExpress;
+    private SawmillExpress sawmillExpress;
     private CollisionData playerSide;
     private CollisionData otherSide;
 
 
     @Before
     public void setup(){
-        player = new Player(new RectangleFactoryAdapterStub());
-        sawmillExpress = (SawmillExpress)npcFactory.make("sawmillExpress", new Point(10, 10)).getModel();
-        playerSide = new CollisionData();
+        IRectangleFactoryAdapter  rectangleFactoryAdapter = new RectangleFactoryAdapterStub();
+        IRepository repository = Mockito.mock(IRepository.class);
+        List<String> list = new ArrayList<>();
+        list.add("sawmill");
+        try {
+            Mockito.when(repository.get("sawmill")).thenReturn(list);
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        this.player = new Player(rectangleFactoryAdapter);
+        this.sawmillExpress = new SawmillExpress(new Point(1,1), rectangleFactoryAdapter, repository, 10, 10 ,10);
+        this.playerSide = new CollisionData();
+        this.otherSide = new CollisionData();
         playerSide.set(Side.RIGHT);
-        otherSide = new CollisionData();
         otherSide.set(Side.LEFT);
     }
 
@@ -40,11 +56,11 @@ public class UC2InteractTest {
      */
     @Test
     public void testInteraction(){
+        assertFalse("Should not talking", sawmillExpress.isDialogVisible());
         player.onCollision(sawmillExpress,playerSide,false);
         sawmillExpress.onCollision(player, otherSide,false);
-        assertFalse("Should not be flying", sawmillExpress.isFlying());
         player.interact();
-        assertTrue("Should be flying", sawmillExpress.isFlying());
+        assertTrue("Should be talking", sawmillExpress.isDialogVisible());
     }
 
     /**
@@ -68,9 +84,8 @@ public class UC2InteractTest {
         player.onCollision(sawmillExpress,playerSide,false);
         sawmillExpress.onCollision(player,otherSide,false);
         assertFalse("Should be false", sawmillExpress.isDialogVisible());
-        assertNull("Should be null", sawmillExpress.getDialog());
         player.interact();
         assertTrue("Should be true", sawmillExpress.isDialogVisible());
-        assertEquals("Should be equal", sawmillExpress.getDialog(), "Wäääh!");
+        assertEquals("Should be equal", sawmillExpress.getDialog(), "sawmill\n");
     }
 }
